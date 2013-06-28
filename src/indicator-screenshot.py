b9d0c9a20c6533a83491deefb621fb0a98382b14
Chen Zhiwei <zhiweik@gmail.com>
2013-06-28 17:57:31 +0800

    update with python setup tool

--- src/indicator-screenshot.py
+++ src/indicator-screenshot.py
@@ -20,13 +20,17 @@
 # <http://www.gnu.org/licenses>
 #
 
-import gtk 
+import os,gtk 
 import appindicator
 import pynotify
-import os
+import dbus
 
 class IndicatorScreenshot:
     def __init__(self, args = None):
+        if dbus.SessionBus().request_name("cn.chenzhiwei.indicator-screenshot") \
+                != dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
+            exit("IndicatorScreenshot Already Running!")
+
         self.args = args
         # Create App Indicator
         self.ind = appindicator.Indicator(
@@ -35,7 +39,7 @@ class IndicatorScreenshot:
         # Delete/modify the following file when distributing as a package
         self.ind.set_icon_theme_path(os.path.abspath(os.path.join(
             os.path.dirname(__file__), 'icons')))
-        self.ind.set_icon("ubuntu-logo-16")
+        self.ind.set_icon("indicator")
         self.ind.set_status(appindicator.STATUS_ACTIVE)
         # Create Menu
         self.menu = gtk.Menu()
@@ -54,14 +58,14 @@ class IndicatorScreenshot:
 
     def select_area(self, *args):
         os.system('deepin-screenshot')
-        uri = "file://" + os.path.abspath(os.path.dirname(__file__)) + "/icons/ubuntu-logo-48.svg"
+        uri = "file://" + os.path.abspath(os.path.dirname(__file__)) + "/icons/screenshot-notification.svg"
         title = 'Screenshot Complete'
         content = 'This is a notification from indicator-screenshot.'
         self.show_notification(title, content, uri)
 
     def full_screen(self, *args):
         os.system('deepin-screenshot -f')
-        uri = "file://" + os.path.abspath(os.path.dirname(__file__)) + "/icons/ubuntu-logo-48.svg"
+        uri = "file://" + os.path.abspath(os.path.dirname(__file__)) + "/icons/screenshot-notification.svg"
         title = 'Screenshot Complete'
         content = 'This is a notification from indicator-screenshot.'
         self.show_notification(title, content, uri)
@@ -79,6 +83,7 @@ def main():
     indicator = IndicatorScreenshot(args)
     # Load global css for the first time.
     gtk.main()
+    exit(0)
 
 if __name__ == "__main__":
     main()
